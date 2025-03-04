57a045d20b6fb7ea16a0f8b6301d7f1e84442b0d
dsk90it <dsk90it@gmail.com>
2025-03-04 20:20:50 +0530

    feat: add shadcn/ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import { Button } from '@/components/ui/button'
+
 function App() {
-  return <>Hello World!</>
+  return (
+    <div className="flex flex-col items-center justify-center min-h-svh">
+      <Button onClick={() => alert('hi')}>Click me</Button>
+    </div>
+  )
 }
 
 export default App
